a875f4d9a3cf244d34000ff01b09461a82f355a8
Sergej Domme <sergej@domme.it>
2015-11-09 15:39:28 +0100

    extend version-number so we can build and upload new RPMs without new commit

--- build.py
+++ build.py
@@ -53,3 +53,5 @@ def set_properties(project):
 
     project.set_property(
         'distutils_console_scripts', ['gaius=gaius.cli:send_message'])
+    project.version = '%s-%s' % (project.version,
+                                 os.environ.get('BUILD_NUMBER', 0))
