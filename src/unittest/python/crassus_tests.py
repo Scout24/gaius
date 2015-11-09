f382864adfb844d4d56c1a7aedc6dca92012b5ac
Sergej Domme <sergej@domme.it>
2015-11-09 12:54:41 +0100

    unset uppercase proxy variables as well

--- src/unittest/python/crassus_tests.py
+++ src/unittest/python/crassus_tests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
-from gaius.crassus import notify_crassus, transform_to_message_format
+from gaius.crassus import notify_crassus
 import boto3
 import os
 from moto import mock_sns
@@ -8,7 +8,10 @@ from moto import mock_sns
 # Else we run into problems with mocking
 os.environ['http_proxy'] = ''
 os.environ['https_proxy'] = ''
+os.environ['HTTP_PROXY'] = ''
+os.environ['HTTPS_PROXY'] = ''
 os.environ['no_proxy'] = ''
+
 
 
 class CrassusTests(TestCase):
@@ -16,7 +19,7 @@ class CrassusTests(TestCase):
     def setUp(self):
         self.my_mock_sns = mock_sns()
         self.my_mock_sns.start()
-        self.sns = boto3.resource('sns', region_name='eu-west-1')
+        self.sns = boto3.resource('sns', region_name="eu-west-1")
         self.topic = self.sns.create_topic(Name='test_sns')
 
     def tearDown(self):
