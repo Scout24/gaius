d63d93f20049af80f072264a0739511b7435d394
Sergej Domme <sergej@domme.it>
2015-11-11 09:30:47 +0100

    There is no boto3 RPM for RHEL6 servers outside. So let's install it with pip during gaius RPM installation as a post task.

--- build.py
+++ build.py
@@ -12,6 +12,7 @@ use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 use_plugin('python.cram')
+use_plugin('copy_resources')
 
 
 name = "gaius"
@@ -41,6 +42,10 @@ def set_properties(project):
     project.set_property("coverage_branch_threshold_warn", 80)
     project.set_property("coverage_branch_partial_threshold_warn", 80)
     project.set_property('coverage_break_build', True)
+
+    project.set_property('copy_resources_target', '$dir_dist')
+    project.get_property('copy_resources_glob').extend(['setup.cfg'])
+    project.get_property('copy_resources_glob').append('post-install.sh')
 
     project.set_property('distutils_classifiers', [
         'Development Status :: 4 - Beta',
