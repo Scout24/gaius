0f65f7bf28e21bb0f715d2a87c2b9731f43fc0c7
crose <carsten.rose@immobilienscout24.de>
2015-11-16 17:25:17 +0100

    add back channel. needs to be tested

--- src/main/python/gaius/cli.py
+++ src/main/python/gaius/cli.py
@@ -1,17 +1,18 @@
 """
 Command line client for deploying CFN stacks via crassus
 Usage:
-    gaius --stack STACK --parameters PARAMETERS --topic-arn ARN [--region REGION]
+    gaius --stack STACK --parameters PARAMETERS --topic-arn ARN [--region REGION] [--back-channel BACK_CHANNEL]
 
 Options:
-  -h --help                Show this.
-  --stack STACK  Stack Name
-  --parameters PARAMETERS  Parameters in format key=value[,key=value]
-  --topic-arn ARN  The ARN of the notify topic
-  --region REGION  the region to deploy in [default: eu-west-1]
+  -h --help                     Show this.
+  --stack STACK                 Stack Name
+  --parameters PARAMETERS       Parameters in format key=value[,key=value]
+  --topic-arn ARN               The ARN of the notify topic to Crassus
+  --region REGION               The region to deploy in [default: eu-west-1]
+  --back-channel BACK_CHANNEL   The name of the back-channel AWS::SQS from Crassus [default: crassus-output]
 """
 
-from gaius import crassus
+from gaius import crassus, back_channel
 from docopt import docopt
 
 
@@ -21,5 +22,7 @@ def send_message():
     parameters = arguments['--parameters']
     topic_arn = arguments['--topic-arn']
     region = arguments['--region']
+    back_channel_name = arguments['--back-channel']
 
     print crassus.notify_crassus(stack_name, parameters, topic_arn, region)
+    back_channel.receive_messages(back_channel_name)
--- src/unittest/python/cli_tests.py
+++ src/unittest/python/cli_tests.py
@@ -7,14 +7,17 @@ from mockito import when, verify, any as any_value, mock
 
 class CliTests(TestCase):
 
-    @patch('gaius.crassus.notify_crassus')
-    def test_send_message(self, notify_crassus_mock):
+    def setUp(self):
         cli.crassus = mock(cli.crassus)
+        cli.back_channel = mock(cli.back_channel)
+
+    def test_send_message(self):
         when(cli).docopt(any_value()).thenReturn({
             '--stack': 'mystack',
             '--parameters': 'parameter1=value1,parameter2=value2',
             '--topic-arn': 'my::topic::arn',
-            '--region': 'eu-west-1'
+            '--region': 'eu-west-1',
+            '--back-channel': 'crassus-output'
         })
 
         cli.send_message()
@@ -25,3 +28,4 @@ class CliTests(TestCase):
             'my::topic::arn',
             'eu-west-1'
         )
+        verify(cli.back_channel).receive_messages('crassus-output')
