253affec970052020fe8cb4204e9e2fbc9954d34
Stefan Neben <stefan.neben@gmail.com>
2015-11-23 13:54:25 +0100

    delete compat module, since we don't need the OrderedDict

--- src/main/python/gaius/compat.py
+++
@@ -1,6 +0,0 @@
-""" Compatability module for different Python versions. """
-
-try:
-    from ordereddict import OrderedDict  # NOQA
-except ImportError:  # pragma: no cover
-    from collections import OrderedDict  # NOQA
