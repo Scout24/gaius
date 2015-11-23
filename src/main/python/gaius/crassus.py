b4235ec058852f061b10dfdcdd00954a219f3496
Stefan Neben <stefan.neben@gmail.com>
2015-11-23 13:58:38 +0100

    add logging for notify

--- src/main/python/gaius/crassus.py
+++ src/main/python/gaius/crassus.py
@@ -1,22 +1,25 @@
+# -*- coding: utf-8 -*-
 """
 Interface to the Crassus Lambda function. This module notifies Crassus
 about updates to a CFN stack so Crassus will trigger the update process.
 """
-# -*- coding: utf-8 -*-
+import logging
 import json
 
 from boto3 import client
+
+logger = logging.Logger('gaius')
 
 
 def notify(stack_name, parameters, topic_arn, region):
     """ Sends an update notification to Crassus """
     message = generate_message(stack_name, parameters, region)
     sns_client = client('sns', region_name=region)
-    json_str = sns_client.publish(
+    json_answer = sns_client.publish(
         TopicArn=topic_arn,
         Message=json.dumps(message),
     )
-    return json_str
+    logger.info(json_answer)
 
 
 def generate_message(stack_name, parameters, region, version=1):
@@ -24,7 +27,6 @@ def generate_message(stack_name, parameters, region, version=1):
     message['version'] = version
     message['stackName'] = stack_name
     message['region'] = region
-
     parameter_list = [x for x in parameters.split(',')]
     parameter_dict = dict([y.split('=') for y in parameter_list])
     message['parameters'] = parameter_dict
