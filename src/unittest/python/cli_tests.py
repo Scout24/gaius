6e56275c52d508bc94644296c7933c64dacd2847
Valentin Haenel <valentin@haenel.co>
2015-11-24 11:11:12 +0100

    fix the cli tests

--- src/unittest/python/cli_tests.py
+++ src/unittest/python/cli_tests.py
@@ -11,14 +11,18 @@ class CliTests(TestCase):
 
     @patch('gaius.service.receive')
     @patch('gaius.service.notify')
-    @patch('docopt.docopt')
+    @patch('gaius.cli.docopt')
     def test_should_communicate(self, mock_docopt, mock_notify, mock_receive):
-        parameters = [
-            '--stack=mystack',
-            '--parameters=parameter1=value1,parameter2=value2',
-            '--topic-arn=my::topic::arn',
-            '--region=eu-west-1',
-            '--back-channel=crassus-output'
-        ]
+        parameters = {
+            '--stack': 'mystack',
+            '--parameters': 'parameter1=value1,parameter2=value2',
+            '--topic-arn': 'my::topic::arn',
+            '--region': 'eu-west-1',
+            '--back-channel': 'crassus-output'
+        }
         mock_docopt.return_value = parameters
         cli.communicate()
+        mock_notify.assert_called_once_with('mystack',
+                                            'parameter1=value1,parameter2=value2',
+                                            'my::topic::arn', 'eu-west-1')
+        mock_receive.assert_called_once_with('crassus-output')
