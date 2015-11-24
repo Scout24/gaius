f2c6bdf26d2b58bb51abf6ac1489efb01c82a854
Stefan Neben <stefan.neben@gmail.com>
2015-11-24 10:56:36 +0100

    Begin with new unit tests after refactoring. Isn't working right now, but switching workspace...

--- src/unittest/python/cli_tests.py
+++ src/unittest/python/cli_tests.py
@@ -1,31 +1,24 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from unittest import TestCase
-from gaius import cli
 from mock import patch
-from mockito import when, verify, any as any_value, mock
+
+from gaius import cli
 
 
 class CliTests(TestCase):
 
-    def setUp(self):
-        cli.crassus = mock(cli.crassus)
-        cli.back_channel = mock(cli.back_channel)
-
-    def test_send_message(self):
-        when(cli).docopt(any_value()).thenReturn({
-            '--stack': 'mystack',
-            '--parameters': 'parameter1=value1,parameter2=value2',
-            '--topic-arn': 'my::topic::arn',
-            '--region': 'eu-west-1',
-            '--back-channel': 'crassus-output'
-        })
-
-        cli.send_message()
-
-        verify(cli.crassus).notify_crassus(
-            'mystack',
-            'parameter1=value1,parameter2=value2',
-            'my::topic::arn',
-            'eu-west-1'
-        )
-        verify(cli.back_channel).receive_messages('crassus-output')
+    @patch('gaius.service.receive')
+    @patch('gaius.service.notify')
+    @patch('docopt.docopt')
+    def test_should_communicate(self, mock_docopt, mock_notify, mock_receive):
+        parameters = [
+            '--stack=mystack',
+            '--parameters=parameter1=value1,parameter2=value2',
+            '--topic-arn=my::topic::arn',
+            '--region=eu-west-1',
+            '--back-channel=crassus-output'
+        ]
+        mock_docopt.return_value = parameters
+        cli.communicate()
