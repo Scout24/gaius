c1ba965f499f63b8872c56a7bd74e0c1d91e2e6c
Valentin Haenel <valentin@haenel.co>
2015-11-24 11:23:35 +0100

    tests for generate message

--- src/unittest/python/service_tests.py
+++ src/unittest/python/service_tests.py
@@ -5,7 +5,7 @@
 from unittest import TestCase
 from mock import patch
 
-from gaius.service import parse_parameters
+from gaius.service import parse_parameters, generate_message
 
 
 class TestParseParameters(TestCase):
@@ -16,3 +16,19 @@ class TestParseParameters(TestCase):
     def test_double_pair_should_be_parsed_correctly(self):
         self.assertEqual({'key1': 'value1', 'key2': 'value2'},
                          parse_parameters('key1=value1,key2=value2'))
+
+
+class TestGenerateMessage(TestCase):
+
+    def test_should_assemble_message_correctly(self):
+        expected = {
+            'version': 1,
+            'stackName': 'ANY_STACK',
+            'region': 'ANY_REGION',
+            'parameters': {'ANY_KEY': 'ANY_VALUE'}
+
+        }
+        received = generate_message('ANY_STACK',
+                                    'ANY_KEY=ANY_VALUE',
+                                    'ANY_REGION')
+        self.assertEqual(expected, received)
